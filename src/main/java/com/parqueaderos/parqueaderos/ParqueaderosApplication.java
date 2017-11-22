dbdfecec06c49b93df668670fbc35aa2c7fa1378
Negrinho1017 <camilo17746@gmail.com>
2017-11-22 09:01:53 -0500

    Otro ensayo

--- src/main/java/com/parqueaderos/parqueaderos/ParqueaderosApplication.java
+++ src/main/java/com/parqueaderos/parqueaderos/ParqueaderosApplication.java
@@ -8,6 +8,5 @@ public class ParqueaderosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParqueaderosApplication.class, args);
-		System.out.println("chao mundo!!");
 	}
 }
